b5ff9d59f64e03474522a48bcf1d6ead6e40018a
守真 孫 <oscarsun72@hotmail.com>
2020-02-07 21:13:57 +0800

    加入專案檔案。

---
+++ 檔案總管汰重-WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using io = System.IO;
+
+namespace 檔案總管汰重_WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (io.Directory.Exists(textBox1.Text) && io.Directory.Exists(textBox2.Text))
+            {
+                foreach (var itemF in io.Directory.GetFiles(textBox1.Text))//iterF(F:from;source)
+                {
+                    io.FileInfo f = new io.FileInfo(itemF);
+                    foreach (var itemT in io.Directory.GetFiles(textBox2.Text))//iterT(T:to;destination)
+                    {
+                        io.FileInfo ft = new io.FileInfo(itemT);
+                        if (f.LastWriteTime == ft.LastWriteTime && f.Length == ft.Length && f.Name == ft.Name)
+                        //if ( f.LastWriteTime==ft.LastWriteTime && f.Length==ft.Length  )//不比對檔名
+                        {
+                            if (io.File.GetAttributes(itemF).ToString().IndexOf(io.FileAttributes.ReadOnly.ToString()) != -1)
+                            {
+                                io.File.SetAttributes(itemF, io.FileAttributes.Normal);
+                            }
+                            io.File.Delete(itemF);//delete from the source file
+                            break;
+                        }
+                    }
+                }
+                if (io.Directory.GetFiles(textBox1.Text).Count() == 0)//if no more files in this directory
+                    io.Directory.Delete(textBox1.Text);                       //then delete this directory
+                MessageBox.Show("done!");
+            }
+            else
+            {
+                MessageBox.Show("路徑有誤！");
+            }
+        }
+
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox1.Text = Clipboard.GetText();
+        }
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+            textBox2.Text = Clipboard.GetText();
+        }
+
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {//https://wijtb.nctu.me/archives/269/
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;//调用DragDrop事件 
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {//https://www.google.com/search?rlz=1C1OKWM_zh-TWTW847TW847&ei=W7wBXbDjG8Kk8AWfkYGoBQ&q=c%23+%E6%8B%96%E6%8B%89%E7%89%A9%E4%BB%B6+%E8%B3%87%E6%96%99%E5%A4%BE&oq=c%23+%E6%8B%96%E6%8B%89%E7%89%A9%E4%BB%B6+%E8%B3%87%E6%96%99&gs_l=psy-ab.3.0.33i160l2.11461.20026..22176...0.0..0.99.232.3......0....1..gws-wiz.......0i30.KqdNNbPjI8A
+            //https://wijtb.nctu.me/archives/269/
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            this.textBox1.Text = filePaths[0];
+        }
+
+        private void textBox2_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
+            this.textBox2.Text = filePaths[0];
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                this.TopMost = true;
+            }
+            else
+            {
+                this.TopMost = false;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+    }
+}
