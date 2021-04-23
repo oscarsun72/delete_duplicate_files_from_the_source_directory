38928135a6662bc113085370f0d8495d25bbc653
守真 孫 <oscarsun72@hotmail.com>
2021-04-23 17:58:03 +0800

    準備改良

---
+++ 檔案總管汰重-WindowsFormsApplication1/ClipBoardPlus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Specialized;
+using System.Windows.Forms;
+
+
+namespace ExplorerFilemanager
+{
+    public static class ClipBoardPlus
+    {
+        public static void CopyFiles(string fileFullname)
+        {
+            //StringCollection A =
+            //        new StringCollection();
+            //A.Add(fileFullname);//此與下等式，VisualStudio建議simple化
+            StringCollection A =new StringCollection{fileFullname};
+            Clipboard.SetFileDropList(A);
+        }
+
+        //https://docs.microsoft.com/zh-tw/dotnet/desktop/winforms/advanced/how-to-add-data-to-the-clipboard?view=netframeworkdesktop-4.8
+        // Demonstrates SetFileDropList, ContainsFileDroList, and GetFileDropList
+        public static System.Collections.Specialized.StringCollection
+            SwapClipboardFileDropList(
+            System.Collections.Specialized.StringCollection replacementList)
+        {
+            System.Collections.Specialized.StringCollection returnList = null;
+            if (Clipboard.ContainsFileDropList())
+            {
+                returnList = Clipboard.GetFileDropList();
+                Clipboard.SetFileDropList(replacementList);
+            }
+            return returnList;
+        }
+
+    }
+}
