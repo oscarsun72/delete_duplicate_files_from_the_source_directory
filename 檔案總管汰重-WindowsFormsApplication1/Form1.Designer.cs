f8ceba79e17ec3f5831f3503d5cf028b16ac2a28
孫守真 <oscarsun72@hotmail.com>
2021-05-08 07:43:32 +0800

    優化程式碼及介面

--- 檔案總管汰重-WindowsFormsApplication1/Form1.Designer.cs
+++ 檔案總管汰重-WindowsFormsApplication1/Form1.Designer.cs
@@ -35,13 +35,17 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(601, 199);
+            this.button1.Location = new System.Drawing.Point(451, 159);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(383, 38);
+            this.button1.Size = new System.Drawing.Size(287, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "執行（或直接按「Enter」鍵即可）";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,22 +54,24 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label1.Location = new System.Drawing.Point(347, 17);
+            this.label1.Location = new System.Drawing.Point(132, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(479, 43);
+            this.label1.Size = new System.Drawing.Size(487, 19);
             this.label1.TabIndex = 1;
-            this.label1.Text = "比較兩個目錄，刪除來源資料夾中相同檔案";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Text = "比較兩個目錄，刪除來源資料夾(上面方塊中所指定者)中相同檔案";
             // 
             // textBox1
             // 
             this.textBox1.AllowDrop = true;
             this.textBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox1.Location = new System.Drawing.Point(45, 58);
+            this.textBox1.ForeColor = System.Drawing.Color.Crimson;
+            this.textBox1.Location = new System.Drawing.Point(123, 46);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(957, 31);
+            this.textBox1.Size = new System.Drawing.Size(777, 27);
             this.textBox1.TabIndex = 2;
-            this.textBox1.Text = "來源資料夾路徑";
+            this.textBox1.Text = "來源資料夾路徑(拖放或點1下貼上即可)";
             this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
             this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
             this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
@@ -74,11 +80,13 @@
             // 
             this.textBox2.AllowDrop = true;
             this.textBox2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.textBox2.Location = new System.Drawing.Point(45, 135);
+            this.textBox2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox2.Location = new System.Drawing.Point(123, 108);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(957, 31);
+            this.textBox2.Size = new System.Drawing.Size(777, 27);
             this.textBox2.TabIndex = 3;
-            this.textBox2.Text = "目的資料夾路徑";
+            this.textBox2.Text = "目的資料夾路徑(拖放或點1下貼上即可)";
             this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
             this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox2_DragDrop);
             this.textBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
@@ -88,9 +96,10 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.checkBox1.ForeColor = System.Drawing.Color.Blue;
-            this.checkBox1.Location = new System.Drawing.Point(373, 198);
+            this.checkBox1.Location = new System.Drawing.Point(317, 169);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(131, 24);
+            this.checkBox1.Size = new System.Drawing.Size(107, 20);
             this.checkBox1.TabIndex = 4;
             this.checkBox1.Text = "最上層顯示";
             this.checkBox1.UseVisualStyleBackColor = true;
@@ -100,18 +109,59 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.label2.Location = new System.Drawing.Point(842, 17);
+            this.label2.Location = new System.Drawing.Point(632, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(139, 24);
+            this.label2.Size = new System.Drawing.Size(111, 19);
             this.label2.TabIndex = 5;
             this.label2.Text = "按Esc鍵關閉";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.checkBox2.ForeColor = System.Drawing.Color.DarkMagenta;
+            this.checkBox2.Location = new System.Drawing.Point(34, 146);
+            this.checkBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(331, 20);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "只比對大小、時間、副標題，檔名不必全同";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(-1, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "來源資料夾\r\n有相同檔則刪除";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(-1, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "目的資料夾\r\n有相同檔則保留";
             // 
             // Form1
             // 
             this.AcceptButton = this.button1;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1036, 256);
+            this.ClientSize = new System.Drawing.Size(911, 205);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox2);
@@ -120,6 +170,7 @@
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "比對並刪除來源資料夾中的檔案";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
@@ -136,6 +187,9 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- 檔案總管汰重-WindowsFormsApplication1/Form1.cs
+++ 檔案總管汰重-WindowsFormsApplication1/Form1.cs
@@ -30,56 +30,55 @@ namespace 檔案總管汰重_WindowsFormsApplication1
             {
                 //foreach (var itemF in io.Directory.GetFiles(textBox1.Text))//iterF(F:from;source)
                 foreach (io.FileInfo itemF in fiArray)//iterF(F:from;source)
-                {
+                {//F=from 來源檔（拿來比較之檔，相同則刪除）;T=to 目的檔（被比較的檔案，相同則保留）;
                     io.FileInfo f = new io.FileInfo(itemF.FullName);
                     //foreach (var itemT in io.Directory.GetFiles(textBox2.Text))//iterT(T:to;destination)
                     foreach (io.FileInfo itemT in fiArray2)//iterT(T:to;destination)
                     {
                         io.FileInfo ft = new io.FileInfo(itemT.FullName);
-                        if (f.LastWriteTime == ft.LastWriteTime && f.Length == ft.Length && f.Name == ft.Name)
-                        //if ( f.LastWriteTime==ft.LastWriteTime && f.Length==ft.Length  )//不比對檔名
+                        if (checkBox2.Checked)//not identical
                         {
-                            //if (io.File.GetAttributes(itemF.FullName).ToString().IndexOf(io.FileAttributes.ReadOnly.ToString()) != -1)
-                            //{
-                            //    io.File.SetAttributes(itemF.FullName, io.FileAttributes.Normal);
-                            //}
-                            NoReadonly(itemF);
-                            io.File.Delete(itemF.FullName);//delete from the source file                            
-                            break;//check the next file in the source directory
+                            if (f.LastWriteTime == ft.LastWriteTime && f.Length == ft.Length && f.Extension == ft.Extension)//不比對檔名，只比對副檔名
+                            {
+                                DeleteFileRemoveReadOnly(itemF);
+                                break;//check the next file in the source(from) directory
+                            }
+                        }
+                        else
+                        {
+                            if (f.LastWriteTime == ft.LastWriteTime && f.Length == ft.Length && f.Name == ft.Name)//比對檔名(identical)
+                            {
+                                //if (io.File.GetAttributes(itemF.FullName).ToString().IndexOf(io.FileAttributes.ReadOnly.ToString()) != -1)
+                                //{
+                                //    io.File.SetAttributes(itemF.FullName, io.FileAttributes.Normal);
+                                //}
+                                DeleteFileRemoveReadOnly(itemF);
+                                break;//check the next file in the source(from) directory
+                            }
                         }
                     }
                 }
-                di = new io.DirectoryInfo(textBox1.Text);
+                //di = new io.DirectoryInfo(textBox1.Text);
                 fiArray = di.GetFiles("*.*", io.SearchOption.AllDirectories);
                 io.DirectoryInfo[] diSubfolders;
                 if (fiArray.Length == 0)
                 {//當沒有檔案，只剩資料夾時
                     diSubfolders = di.GetDirectories("*.*", io.SearchOption.AllDirectories);
                     if (diSubfolders.Length == 0)
-                    {
+                    {//如果沒有子資料夾/子目錄時，就直接刪除處理的資料夾目錄：
                         io.Directory.Delete(textBox1.Text); //if no more files in this directory then delete this directory
                                                             //若但用 if (io.Directory.GetFiles(textBox1.Text).Count() == 0）來判斷，則當尚有子目錄時會出錯
                     }
                     else
-                    {
+                    {//如果有子資料夾/子目錄時，須逐一清空各資料夾目錄（當某一資料夾含有子目錄時，不能直接將其刪除）：
                         diSubfolders = ClearEmptyFolders(di, diSubfolders);
                     }
                 }
                 else
                 {//還有檔案，又有空資料夾時
-                    di = new io.DirectoryInfo(textBox1.Text);
+                    //di = new io.DirectoryInfo(textBox1.Text);
                     diSubfolders = di.GetDirectories("*.*", io.SearchOption.AllDirectories);
                     ClearEmptyFolders(di, diSubfolders);
-                    //IEnumerable<io.DirectoryInfo> subDi =
-                    //    from subD in diSubfolders
-                    //    where subD.GetFiles("*.*", io.SearchOption.AllDirectories).Length == 0
-                    //    select subD;
-                    //foreach (io.DirectoryInfo sDi in subDi)
-                    //{
-                    //clearEmptyFolders(sDi, sDi.GetDirectories
-                    //    ("*.*", io.SearchOption.AllDirectories));
-                    //}
-
                 }
                 MessageBox.Show("done!");
             }
@@ -87,6 +86,14 @@ namespace 檔案總管汰重_WindowsFormsApplication1
             {
                 MessageBox.Show("路徑有誤！");
             }
+        }
+
+        //直接刪除檔案，不管有沒有唯讀屬性 20210508
+        public static void DeleteFileRemoveReadOnly(FileInfo fileInfo)
+        {
+            NoReadonly(fileInfo);
+            fileInfo.Delete();//上下兩式作用相同
+            //io.File.Delete(fileInfo.FullName);//delete from the source file                            
         }
 
         public static DirectoryInfo[] ClearEmptyFolders(DirectoryInfo di,
@@ -160,7 +167,7 @@ namespace 檔案總管汰重_WindowsFormsApplication1
         }
         public static void NoReadonly(DirectoryInfo di)
         {
-            if(IsReadonly(di)) di.Attributes &= ~io.FileAttributes.ReadOnly;
+            if (IsReadonly(di)) di.Attributes &= ~io.FileAttributes.ReadOnly;
         }
         public static void NoReadonly(FileInfo fi)
         {
@@ -201,11 +208,6 @@ namespace 檔案總管汰重_WindowsFormsApplication1
         {
             string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop);
             this.textBox2.Text = filePaths[0];
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
